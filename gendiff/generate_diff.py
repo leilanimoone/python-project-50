fd2913f38d2b44f38092fbcde7d2300c4b53359d
leilanimoone <leilanimoone@gmail.com>
2023-03-13 16:48:20 +0300

    fix generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,6 +1,15 @@
 from gendiff.data import prepare_data
 
 
+def is_bool(value):
+    if str(value) == 'True':
+        return 'true'
+    elif str(value) == 'False':
+        return 'false'
+    else:
+        return value
+
+
 def generate_diff(first_file, second_file):
     data1 = prepare_data(first_file)
     data2 = prepare_data(second_file)
@@ -8,12 +17,12 @@ def generate_diff(first_file, second_file):
     sorted_keys = sorted(set(data1.keys()) | set(data2.keys()))
     for key in sorted_keys:
         if key not in data1:
-            diff += f'+ {key}: {data2[key]}\n'
+            diff += f'  + {key}: {is_bool(data2[key])}\n'
         elif key not in data2:
-            diff += f'- {key}: {data1[key]}\n'
+            diff += f'  - {key}: {is_bool(data1[key])}\n'
         elif data1[key] == data2[key]:
-            diff += f'  {key}: {data1[key]}\n'
+            diff += f'    {key}: {is_bool(data1[key])}\n'
         elif data1[key] != data2[key]:
-            diff += f'- {key}: {data1[key]}\n'
-            diff += f'+ {key}: {data2[key]}\n'
+            diff += f'  - {key}: {is_bool(data1[key])}\n'
+            diff += f'  + {key}: {is_bool(data2[key])}\n'
     return '{\n' + diff + '}'
