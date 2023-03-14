93b38b6611961c9b60950b7a42a8fd8fcebd309d
leilanimoone <leilanimoone@gmail.com>
2023-03-14 11:41:41 +0300

    update tests

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -1,16 +1,19 @@
 import pytest
 from gendiff.generate_diff import generate_diff
-from gendiff.data import prepare_data
 
 
 json1 = 'tests/fixtures/file1.json'
 json2 = 'tests/fixtures/file2.json'
 result_json = 'tests/fixtures/result_json.json'
+yml1 = 'tests/fixtures/file1.yml'
+yml2 = 'tests/fixtures/file2.yml'
+result_yml = 'tests/fixtures/result_yml.yml'
 
 
 @pytest.mark.parametrize(
                         'file1, file2, result', 
-                        [(json1, json2, result_json)]
+                        [(json1, json2, result_json),
+                        (yml1, yml2, result_yml)]
 )
 def test_gendiff(file1, file2, result):
     with open(result) as res:
