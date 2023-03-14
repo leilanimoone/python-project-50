051454c91a75fb6816bcdc8fc3a5b15bb5174bc4
leilanimoone <leilanimoone@gmail.com>
2023-03-14 11:42:27 +0300

    add new format

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,28 +1,9 @@
 from gendiff.data import prepare_data
+from gendiff.data import get_format
+from gendiff.get_diff import get_diff
 
 
-def is_bool(value):
-    if str(value) == 'True':
-        return 'true'
-    elif str(value) == 'False':
-        return 'false'
-    else:
-        return value
-
-
-def generate_diff(first_file, second_file):
-    data1 = prepare_data(first_file)
-    data2 = prepare_data(second_file)
-    diff = str()
-    sorted_keys = sorted(set(data1.keys()) | set(data2.keys()))
-    for key in sorted_keys:
-        if key not in data1:
-            diff += f'  + {key}: {is_bool(data2[key])}\n'
-        elif key not in data2:
-            diff += f'  - {key}: {is_bool(data1[key])}\n'
-        elif data1[key] == data2[key]:
-            diff += f'    {key}: {is_bool(data1[key])}\n'
-        elif data1[key] != data2[key]:
-            diff += f'  - {key}: {is_bool(data1[key])}\n'
-            diff += f'  + {key}: {is_bool(data2[key])}\n'
-    return '{\n' + diff + '}'
+def generate_diff(file1, file2):
+    data1 = prepare_data(file1, get_format(file1))
+    data2 = prepare_data(file2, get_format(file2))
+    return get_diff(data1, data2)
