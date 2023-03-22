d17c0c83fb791c2dd3d5d268a72a52636efe7569
leilanimoone <leilanimoone@gmail.com>
2023-03-22 17:40:22 +0300

    fix

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -12,13 +12,14 @@ result_json = 'tests/fixtures/result_json'
 
 formaters = ['stylish', 'plain', 'json']
 
-@pytest.mark.parametrize('file1, file2, result, format_name', 
-                        [(json1, json2, result_stylish, formaters[0]),
-                        (json1, json2, result_plain, formaters[1]),
-                        (json1, json2, result_json, formaters[2]),
-                        (yml1, yml2, result_stylish, formaters[0]),
-                        (yml1, yml2, result_plain, formaters[1]),
-                        (yml1, yml2, result_json, formaters[2])])
+
+@pytest.mark.parametrize('file1, file2, result, format_name',
+                         [(json1, json2, result_stylish, formaters[0]),
+                          (json1, json2, result_plain, formaters[1]),
+                          (json1, json2, result_json, formaters[2]),
+                          (yml1, yml2, result_stylish, formaters[0]),
+                          (yml1, yml2, result_plain, formaters[1]),
+                          (yml1, yml2, result_json, formaters[2])])
 def test_gendiff(file1, file2, result, format_name):
     with open(result) as res:
         assert generate_diff(file1, file2, format_name) == res.read()
